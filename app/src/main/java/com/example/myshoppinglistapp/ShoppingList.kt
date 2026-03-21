d432d4f114e8283974f00d188ce76ee5dc18d927
shashibrvsingh <shashi.brvsingh@gmail.com>
2026-03-21 08:38:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myshoppinglistapp/ShoppingList.kt
@@ -0,0 +1,108 @@
+package com.example.myshoppinglistapp
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
+
+data class  ShoppingItem(val id: Int, val name: String, var quantity: Int, var isEditing: Boolean= false )
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShoppingListApp(){
+    var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
+    var showDialog by remember { mutableStateOf(false) }
+    var itemName by remember { mutableStateOf("") }
+    var itemQuality by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top // Changed from Center so you can see it at the top
+    ) {
+        Button(
+            onClick = { showDialog = true },
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Blue,     // Background color
+                contentColor = Color.White       // Text color
+            )
+        ) {
+            Text("Add Items")
+        }
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize().padding(40.dp)
+        ) {
+            // Your items here
+            items(sItems){
+
+            }
+        }
+    }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            confirmButton = {
+                 Row(modifier = Modifier.fillMaxWidth().padding(8.dp),
+                     horizontalArrangement = Arrangement.SpaceAround
+                     ){
+
+                     Button(onClick = {
+                         if(itemName.isNotBlank()){
+                             val newItem = ShoppingItem(id = sItems.size+1,
+                                 name = itemName,
+                                 quantity = itemQuality.toInt())
+                         sItems = sItems +  newItem
+
+                         itemName= ""
+                         itemQuality= ""
+                             showDialog= false
+                         }
+                     })
+
+                     { Text("Add")}
+                     Button(onClick = {showDialog= false}) { Text("Cancel")}
+                 }
+            },
+            title = { Text("Add Item") },
+            text = {
+                 Column {
+                     OutlinedTextField(value = itemName ,
+                         onValueChange = {itemName= it},
+                        singleLine = true,
+                         modifier = Modifier.fillMaxWidth().padding(8.dp)
+                     )
+
+                     OutlinedTextField(value = itemQuality ,
+                         onValueChange = {itemQuality= it},
+                         singleLine = true,
+                         modifier = Modifier.fillMaxWidth().padding(8.dp)
+                     )
+                 }
+            }
+        )
+    }
+}
